55cef95e5d080d3e55f3c6cb4300f52a4bd4ef10
Krittiya20scoops <krittiya.s@20scoops.net>
2023-07-12 13:13:46 +0700

    update typescript converted from js

---
+++ index.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+// import routes from "./src/routes/routes"
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error: any) => {
+    // tslint:disable-next-line:no-console
+    console.log(error)
+})
+
+database.once('connected', () => {
+    // tslint:disable-next-line:no-console
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+
+// app.use('/api', routes)
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+  });
+
+app.listen(3000, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Server Started ts at ${3000}`)
+})
